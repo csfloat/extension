9c301754598c25412b3f4c513343463b97b36534
Step7750 <step7750@gmail.com>
2021-05-03 20:27:34 -0600

    Adds CSGOFloat Auction Banner

--- float.js
+++ float.js
@@ -293,57 +293,24 @@ const addFloatUtilities = async function() {
 
     document.querySelector('#searchResultsTable').insertBefore(parentDiv, document.querySelector('#searchResultsRows'));
 
+    // Add Banner
+    const bannerDiv = document.createElement('div');
+    bannerDiv.id = 'floatBanner';
 
-    // Add CS.Money prices
-    const csmoneyDiv = document.createElement('div');
-    csmoneyDiv.id = 'floatMoney';
+    const link = document.createElement('a');
+    link.href = 'https://csgofloat.com';
+    link.target = '_blank';
 
-    const moneyButton = document.createElement('a');
-    const moneyLogo = document.createElement('img');
-    moneyLogo.src = 'https://cs.money/svg/logo.svg';
-    moneyLogo.height = 32;
+    const bannerLogo = document.createElement('img');
+    bannerLogo.src = 'https://csgofloat.com/assets/extension_banner.png';
+    bannerLogo.height = 75;
 
-    const staticText = document.createElement('span');
-    staticText.innerText = 'Get this skin on ';
-    staticText.style.verticalAlign = 'bottom';
-
-    const priceText = document.createElement('span');
-    const price = document.createElement('span');
-    price.innerText = '$X.XX';
-    price.style.fontWeight = 'bold';
-    price.style.verticalAlign = 'bottom';
-
-    priceText.appendChild(price);
-    priceText.insertAdjacentText('afterbegin', ' for ');
-    priceText.insertAdjacentText('beforeend', ' USD');
-
-    priceText.style.verticalAlign = 'bottom';
-
-    moneyButton.appendChild(staticText);
-    moneyButton.appendChild(moneyLogo);
-    moneyButton.appendChild(priceText);
-    moneyButton.classList.add('float-money-button');
-    csmoneyDiv.appendChild(moneyButton);
-
-    const itemName = await getPageMarketHashName();
-    moneyButton.href = 'https://cs.money/?utm_source=sponsorship&utm_medium=csgoflt&utm_campaign=csgofloat&utm_content=link';
-    moneyButton.target = '_blank';
-
-    // Fetch the current price on CS.Money
-    const data = await sendMessage({ name: itemName, price: true });
-    if (data.trade && data.buy) {
-        price.innerText = `$${data.buy.toFixed(2)}`;
-    } else {
-        priceText.innerText = '';
-    }
-
-    if (data.link) {
-        moneyButton.href = data.link;
-    }
+    link.appendChild(bannerLogo);
+    bannerDiv.appendChild(link);
 
     document
         .querySelector('#searchResultsTable')
-        .insertBefore(csmoneyDiv, document.querySelector('#searchResultsRows'));
+        .insertBefore(bannerDiv, document.querySelector('#searchResultsRows'));
 };
 
 const removeInventoryMods = function(parent) {
