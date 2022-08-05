5608527d323d5aca17f43d7ee5dc10723e1708a0
Step7750 <step7750@gmail.com>
2022-08-05 00:53:50 -0600

    Restricts Callers of Execute Script to Extension Origin

--- src/lib/bridge/handlers/execute_script.ts
+++ src/lib/bridge/handlers/execute_script.ts
@@ -1,17 +1,16 @@
-import {EmptyResponseHandler, RequestType} from "./main";
+import {EmptyResponseHandler, RequestType, RestrictedHandler} from "./main";
 
 interface ExecuteScriptRequest {
     path: string;
 }
 
-export const ExecuteScriptOnPage = new EmptyResponseHandler<ExecuteScriptRequest>(
+export const ExecuteScriptOnPage = new RestrictedHandler(new EmptyResponseHandler<ExecuteScriptRequest>(
     RequestType.EXECUTE_SCRIPT_ON_PAGE,
     async (req, sender) => {
-        // VALIDATE THE PATH!!!
         await chrome.scripting.executeScript(
             {
                 target: {tabId: sender.tab?.id as number},
                 files: [req.path],
                 world: 'MAIN'
             });
-    });
+    }));
--- src/lib/bridge/handlers/main.ts
+++ src/lib/bridge/handlers/main.ts
@@ -1,5 +1,6 @@
 import {RequestHandler} from "../types";
 import MessageSender = chrome.runtime.MessageSender;
+import {EXTENSION_ID} from "../../constants";
 
 export enum RequestType {
     EXECUTE_SCRIPT_ON_PAGE,
@@ -43,5 +44,25 @@ export class EmptyResponseHandler<Req> implements RequestHandler<Req, void> {
 
     handleRequest(request: Req, sender: MessageSender): Promise<void> {
         return this.handler(request, sender);
+    }
+}
+
+/**
+ * Restricts a given handler such that it can only run if the sender is
+ * verified to be from the extension's origin (ie. content script)
+ */
+export class RestrictedHandler<Req, Resp> implements RequestHandler<Req, Resp> {
+    constructor(private handler: RequestHandler<Req, Resp>) {}
+
+    getType(): RequestType {
+        return this.handler.getType();
+    }
+
+    handleRequest(request: Req, sender: MessageSender): Promise<Resp> {
+        if (sender.id !== EXTENSION_ID) {
+            throw new Error('Attempt to access restricted method outside of secure context (ie. content script)');
+        }
+
+        return this.handler.handleRequest(request, sender);
     }
 }
