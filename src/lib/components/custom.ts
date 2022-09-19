31723e53d1dbe13747fde612da46fa3f1699f299
Step7750 <step7750@gmail.com>
2022-09-19 00:48:55 -0600

    Adds Ability to Customize View Encapsulation Type

--- src/lib/components/custom.ts
+++ src/lib/components/custom.ts
@@ -4,13 +4,28 @@ function camelToDashCase(str: string) {
     return str.split(/(?=[A-Z])/).join('-').toLowerCase();
 }
 
+export enum ViewEncapsulation {
+    NONE,
+    SHADOW_DOM,
+}
+
 // LitElement wrapper with a pre-determined tag
 export class FloatElement extends LitElement {
+    protected encapsulation: ViewEncapsulation = ViewEncapsulation.SHADOW_DOM;
+
     static tag(): string {
         return `csgofloat-${camelToDashCase(this.name)}`;
     }
 
     static elem(): any {
         return document.createElement(this.tag());
+    }
+
+    protected createRenderRoot(): Element | ShadowRoot {
+        if (this.encapsulation === ViewEncapsulation.NONE) {
+            return this;
+        } else {
+            return super.createRenderRoot();
+        }
     }
 }
