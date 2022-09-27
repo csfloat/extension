22b9581337010d94c922b3aeb16c9cd651264c27
Step7750 <step7750@gmail.com>
2022-09-27 00:48:28 -0600

    Removes FetchGlobalFilters Stub

--- src/lib/bridge/handlers/handlers.ts
+++ src/lib/bridge/handlers/handlers.ts
@@ -1,5 +1,4 @@
 import {ExecuteScriptOnPage} from "./execute_script";
-import {FetchGlobalFilters} from "./fetch_global_filters";
 import {FetchStall} from "./fetch_stall";
 import {FetchInspectInfo} from "./fetch_inspect_info";
 import {ExecuteCssOnPage} from "./execute_css";
@@ -10,7 +9,6 @@ export const HANDLERS = [
     ExecuteScriptOnPage,
     ExecuteCssOnPage,
     FetchInspectInfo,
-    FetchGlobalFilters,
     FetchStall,
     StorageGet,
     StorageSet,
