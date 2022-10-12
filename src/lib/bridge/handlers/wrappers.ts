0fb29b4a87d1d9d315c5ea7fb8057ae51a0a18ef
Step7750 <step7750@gmail.com>
2022-10-11 22:25:48 -0600

    Lint Wrappers

--- src/lib/bridge/handlers/wrappers.ts
+++ src/lib/bridge/handlers/wrappers.ts
@@ -1,24 +1,24 @@
-import { Job, TTLCachedQueue } from "../../utils/queue";
-import stringify from "fast-json-stable-stringify";
-import { RequestHandler } from "../types";
-import { RequestType } from "./handlers";
+import {Job, TTLCachedQueue} from '../../utils/queue';
+import stringify from 'fast-json-stable-stringify';
+import {RequestHandler} from '../types';
+import {RequestType} from './handlers';
 import MessageSender = chrome.runtime.MessageSender;
 
 interface WrappedRequest<Req> {
-  data: Req;
-  sender: MessageSender;
+    data: Req;
+    sender: MessageSender;
 }
 
 class HandlerJob<Req> extends Job<WrappedRequest<Req>> {
-  constructor(protected req: Req, private sender: MessageSender) {
-    super({data: req, sender});
-  }
+    constructor(protected req: Req, private sender: MessageSender) {
+        super({data: req, sender});
+    }
 
-  hashCode(): string {
-    // Ensure deterministic stringification between two requests for the same
-    // properties
-    return stringify(this.data);
-  }
+    hashCode(): string {
+        // Ensure deterministic stringification between two requests for the same
+        // properties
+        return stringify(this.data);
+    }
 }
 
 /**
@@ -26,17 +26,17 @@ class HandlerJob<Req> extends Job<WrappedRequest<Req>> {
  * extra properties (ie. sender) in addition to the data.
  */
 class HandlerQueue<Req, Resp> extends TTLCachedQueue<WrappedRequest<Req>, Resp> {
-  constructor(maxConcurrency: number, ttlMs: number, private handler: RequestHandler<Req, Resp>) {
-    super(maxConcurrency, ttlMs);
-  }
+    constructor(maxConcurrency: number, ttlMs: number, private handler: RequestHandler<Req, Resp>) {
+        super(maxConcurrency, ttlMs);
+    }
 
-  fetch(req: Req, sender: MessageSender): Promise<Resp> {
-    return this.add(new HandlerJob<Req>(req, sender);
-  }
+    fetch(req: Req, sender: MessageSender): Promise<Resp> {
+        return this.add(new HandlerJob<Req>(req, sender));
+    }
 
-  protected process(req: WrappedRequest<Req>): Promise<Resp> {
-    return this.handler.handleRequest(req.data, req.sender);
-  }
+    protected process(req: WrappedRequest<Req>): Promise<Resp> {
+        return this.handler.handleRequest(req.data, req.sender);
+    }
 }
 
 /**
@@ -46,19 +46,19 @@ class HandlerQueue<Req, Resp> extends TTLCachedQueue<WrappedRequest<Req>, Resp> 
  * If there are multiple in-flight requests for the same
  */
 export class CachedHandler<Req, Resp> implements RequestHandler<Req, Resp> {
-  private queue: HandlerQueue<Req, Resp>;
+    private queue: HandlerQueue<Req, Resp>;
 
-  constructor(private handler: RequestHandler<Req, Resp>, maxConcurrency: number, ttlMs: number) {
-    this.queue = new HandlerQueue<Req, Resp>(Number.MAX_VALUE, ttlMs, handler);
-  }
+    constructor(private handler: RequestHandler<Req, Resp>, maxConcurrency: number, ttlMs: number) {
+        this.queue = new HandlerQueue<Req, Resp>(Number.MAX_VALUE, ttlMs, handler);
+    }
 
-  getType(): RequestType {
-    return this.handler.getType();
-  }
+    getType(): RequestType {
+        return this.handler.getType();
+    }
 
-  handleRequest(request: Req, sender: MessageSender): Promise<Resp> {
-    return this.queue.fetch(request, sender);
-  }
+    handleRequest(request: Req, sender: MessageSender): Promise<Resp> {
+        return this.queue.fetch(request, sender);
+    }
 }
 
 /**
@@ -66,18 +66,17 @@ export class CachedHandler<Req, Resp> implements RequestHandler<Req, Resp> {
  * verified to be from the extension's origin (ie. content script)
  */
 export class PrivilegedHandler<Req, Resp> implements RequestHandler<Req, Resp> {
-  constructor(private handler: RequestHandler<Req, Resp>) {
-  }
+    constructor(private handler: RequestHandler<Req, Resp>) {}
 
-  getType(): RequestType {
-    return this.handler.getType();
-  }
-
-  handleRequest(request: Req, sender: MessageSender): Promise<Resp> {
-    if (sender.id !== chrome.runtime.id) {
-      throw new Error("Attempt to access restricted method outside of secure context (ie. content script)");
+    getType(): RequestType {
+        return this.handler.getType();
     }
 
-    return this.handler.handleRequest(request, sender);
-  }
+    handleRequest(request: Req, sender: MessageSender): Promise<Resp> {
+        if (sender.id !== chrome.runtime.id) {
+            throw new Error('Attempt to access restricted method outside of secure context (ie. content script)');
+        }
+
+        return this.handler.handleRequest(request, sender);
+    }
 }
