0288dbcdbabda4319e1f33c2e625d9dfc0679d43
ayoung <zeusandninja@gmail.com>
2023-07-14 05:30:04 -0700

    Add sorting by fade

--- src/lib/components/market/sort_floats.ts
+++ src/lib/components/market/sort_floats.ts
@@ -6,6 +6,13 @@ import {state} from 'lit/decorators.js';
 import {gFloatFetcher} from '../../services/float_fetcher';
 import {getMarketInspectLink} from './helpers';
 import {ItemInfo} from '../../bridge/handlers/fetch_inspect_info';
+import {getFadePercentage} from '../../utils/skin';
+import {AppId, ContextId} from '../../types/steam_constants';
+
+enum SortType {
+    FLOAT = 'Float',
+    FADE = 'Fade',
+}
 
 enum SortDirection {
     NONE,
@@ -16,15 +23,18 @@ enum SortDirection {
 @CustomElement()
 export class SortFloats extends FloatElement {
     @state()
+    private type: SortType = SortType.FLOAT;
+    @state()
     private direction: SortDirection = SortDirection.NONE;
 
-    @state()
-    get buttonText(): string {
-        let txt = 'Sort by Float';
+    computeButtonText(sortType: SortType): string {
+        let txt = `Sort by ${sortType}`;
 
-        if (this.direction === SortDirection.ASC) {
+        const sortDirection = sortType === this.type ? this.direction : SortDirection.NONE;
+
+        if (sortDirection === SortDirection.ASC) {
             txt += ' ▲';
-        } else if (this.direction === SortDirection.DESC) {
+        } else if (sortDirection === SortDirection.DESC) {
             txt += ' ▼';
         }
 
@@ -33,54 +43,80 @@ export class SortFloats extends FloatElement {
 
     protected render(): HTMLTemplateResult {
         return html`
-            <csgofloat-steam-button .text="${this.buttonText}" @click="${this.onClick}"></csgofloat-steam-button>
+            <csgofloat-steam-button
+                .text="${this.computeButtonText(SortType.FLOAT)}"
+                @click="${() => this.onClick(SortType.FLOAT)}"
+            ></csgofloat-steam-button>
+            <csgofloat-steam-button
+                .text="${this.computeButtonText(SortType.FADE)}"
+                @click="${() => this.onClick(SortType.FADE)}"
+            ></csgofloat-steam-button>
         `;
     }
 
-    getNextSortDirection(): SortDirection {
-        switch (this.direction) {
+    static getNextSortDirection(sortDirection: SortDirection): SortDirection {
+        switch (sortDirection) {
             case SortDirection.NONE:
                 return SortDirection.ASC;
             case SortDirection.ASC:
                 return SortDirection.DESC;
             case SortDirection.DESC:
-                return SortDirection.ASC;
+                return SortDirection.NONE;
         }
     }
 
     static sort(
-        infos: {listingId: string; info: ItemInfo}[],
+        infos: {listingId: string; info: ItemInfo; converted_price: number; fadePercentage: number}[],
+        sortType: SortType,
         direction: SortDirection
     ): {listingId: string; info: ItemInfo}[] {
+        const floatOrFade = (float: number, fade: number) => (sortType === SortType.FLOAT ? float : fade);
+
         switch (direction) {
             case SortDirection.NONE:
-                return infos;
+                return infos.sort((a, b) => a.converted_price - b.converted_price);
             case SortDirection.ASC:
-                return infos.sort((a, b) => a.info.floatvalue - b.info.floatvalue);
+                return infos.sort(
+                    (a, b) =>
+                        floatOrFade(a.info.floatvalue, a.fadePercentage) -
+                        floatOrFade(b.info.floatvalue, b.fadePercentage)
+                );
             case SortDirection.DESC:
-                return infos.sort((a, b) => b.info.floatvalue - a.info.floatvalue);
+                return infos.sort(
+                    (a, b) =>
+                        floatOrFade(b.info.floatvalue, b.fadePercentage) -
+                        floatOrFade(a.info.floatvalue, a.fadePercentage)
+                );
         }
     }
 
-    async onClick() {
-        const newDirection = this.getNextSortDirection();
+    async onClick(sortType: SortType) {
+        const newDirection =
+            sortType == this.type ? SortFloats.getNextSortDirection(this.direction) : SortDirection.ASC;
 
         const rows = document.querySelectorAll('#searchResultsRows .market_listing_row.market_recent_listing_row');
 
-        const infoPromises: Promise<{listingId: string; info: ItemInfo}>[] = [...rows]
+        const infoPromises = [...rows]
             .map((e) => e.id.replace('listing_', ''))
             .map(async (listingId) => {
                 const link = getMarketInspectLink(listingId);
 
                 const info = await gFloatFetcher.fetch({link: link!});
+
+                const listingInfo = g_rgListingInfo[listingId];
+
+                const asset = g_rgAssets[AppId.CSGO][ContextId.PRIMARY][listingInfo.asset.id];
+
                 return {
                     info,
                     listingId: listingId!,
+                    converted_price: listingInfo?.converted_price || 0,
+                    fadePercentage: (asset && getFadePercentage(asset, info)) || 0,
                 };
             });
 
-        const infos: {listingId: string; info: ItemInfo}[] = await Promise.all(infoPromises);
-        const sortedInfos = SortFloats.sort(infos, newDirection);
+        const infos = await Promise.all(infoPromises);
+        const sortedInfos = SortFloats.sort(infos, sortType, newDirection);
 
         let lastItem = document.querySelector('#searchResultsRows .market_listing_table_header');
 
@@ -89,6 +125,7 @@ export class SortFloats extends FloatElement {
             lastItem = itemElement!.parentNode!.insertBefore(itemElement!, lastItem!.nextSibling);
         }
 
+        this.type = sortType;
         this.direction = newDirection;
     }
 }
