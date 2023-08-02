00497633ce446b56d0cf6ea6ed2d938c981ddf43
Step7750 <step7750@gmail.com>
2023-08-01 19:25:54 -0600

    Fixes Extracting API Key for Non-English Pages

--- src/lib/utils/key.ts
+++ src/lib/utils/key.ts
@@ -7,7 +7,7 @@ export async function fetchRegisteredSteamAPIKey(): Promise<string> {
     const pageResponse = await fetch('https://steamcommunity.com/dev/apikey');
     const pageText = await pageResponse.text();
 
-    const match = pageText.match(/Key: ([0-9A-Z]{32})[^0-9A-Z]/);
+    const match = pageText.match(/: ([0-9A-Z]{32})[^0-9A-Z]/);
     if (match) {
         return match[1];
     }
