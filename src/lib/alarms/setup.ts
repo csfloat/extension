51caf01737208b13d3ca3caccdc698b5a5007c91
Step7750 <step7750@gmail.com>
2024-05-03 22:12:59 -0600

    prevents possible undefined deference

--- src/lib/alarms/setup.ts
+++ src/lib/alarms/setup.ts
@@ -24,8 +24,8 @@ export async function registerTradeAlarmIfPossible() {
     const alarm = await chrome.alarms.get(PING_CSFLOAT_TRADE_STATUS_ALARM_NAME);
 
     const hasAlarmWithOutdatedTimer =
-        (alarm?.periodInMinutes && alarm.periodInMinutes > 3) ||
-        (alarm.scheduledTime && alarm.scheduledTime > Date.now() + 10 * 60 * 1000); // Alarm scheduled more than 10 minutes in the future (can be caused by bad system clock)
+        (alarm?.periodInMinutes && alarm?.periodInMinutes > 3) ||
+        (alarm?.scheduledTime && alarm?.scheduledTime > Date.now() + 10 * 60 * 1000); // Alarm scheduled more than 10 minutes in the future (can be caused by bad system clock)
 
     if (!alarm || hasAlarmWithOutdatedTimer) {
         await chrome.alarms.create(PING_CSFLOAT_TRADE_STATUS_ALARM_NAME, {
