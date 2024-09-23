e4066ae9600abe4974c0bfc1a30e39f41fa1b4c6
Step7750 <step7750@gmail.com>
2024-09-23 16:52:32 -0600

    Fixes Refreshing Steam JWT Upon Fetch

--- src/lib/bridge/handlers/fetch_steam_user.ts
+++ src/lib/bridge/handlers/fetch_steam_user.ts
@@ -12,7 +12,12 @@ interface FetchSteamUserResponse {
 export const FetchSteamUser = new SimpleHandler<FetchSteamUserRequest, FetchSteamUserResponse>(
     RequestType.FETCH_STEAM_USER,
     async (req) => {
-        const resp = await fetch('https://steamcommunity.com');
+        const resp = await fetch('https://steamcommunity.com', {
+            headers: {
+                // Required for Steam to refresh the JWT when it expires
+                Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            },
+        });
         if (!resp.ok) {
             throw new Error('non-ok response for steamcommunity.com');
         }
