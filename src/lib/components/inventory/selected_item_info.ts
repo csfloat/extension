27689f82acfc9162f7f44e2e5bf8b52970e241a8
GODrums <armin.stanitzok@outlook.de>
2024-11-20 05:11:25 +0100

    First floatbar design

--- src/lib/components/inventory/selected_item_info.ts
+++ src/lib/components/inventory/selected_item_info.ts
@@ -52,6 +52,28 @@ export class SelectedItemInfo extends FloatElement {
                 color: #ebebeb;
                 text-decoration: none;
             }
+
+            .market-float-bar-container {
+                position: relative;
+                width: 100%;
+                height: 8px; 
+                margin: 5px 0;
+            }
+
+            .market-float-bar-marker {
+                position: absolute; 
+                background-color: #d9d9d9; 
+                width: 3px;
+                top: -3px;
+                height: 14px;
+                border-radius: 4px;
+            }
+
+            .market-float-bar {
+                display: inline-block; 
+                vertical-align: top; 
+                height: 100%; 
+            }
         `,
     ];
 
@@ -101,6 +123,7 @@ export class SelectedItemInfo extends FloatElement {
         const containerChildren: TemplateResult[] = [];
 
         if (isSkin(this.asset.description) && this.itemInfo) {
+            containerChildren.push(this.renderFloatBar());
             containerChildren.push(
                 html`<div>Float: ${this.itemInfo.floatvalue.toFixed(14)} ${renderClickableRank(this.itemInfo)}</div>`
             );
@@ -128,6 +151,40 @@ export class SelectedItemInfo extends FloatElement {
         }
 
         return html` <div class="container">${containerChildren}</div> `;
+    }
+
+    renderFloatBar(): TemplateResult<1> {
+        if (!this.itemInfo) {
+            return html``;
+        }
+        const itemInfo = this.itemInfo;
+        const left = (this.itemInfo.min * 100).toFixed(0);
+        const width = ((this.itemInfo.max - this.itemInfo.min) * 100).toFixed(0);
+        const markerLeft = (100 * this.itemInfo.floatvalue / (this.itemInfo.max - this.itemInfo.min)).toFixed(2);
+
+        let dynamicWidth = (this.itemInfo.max - this.itemInfo.min) * 100;
+        const floatConditions = [
+            { min: 0, max: 6, color: 'green' },
+            { min: 6, max: 15, color: '#18a518' },
+            { min: 15, max: 38, color: '#9acd32' },
+            { min: 38, max: 45, color: '#cd5c5c' },
+            { min: 45, max: 100, color: '#f92424' },
+        ];
+
+        return html`
+            <div class="market-float-bar-container" style="left: ${left}%; width: ${width}%;">
+                <div class="market-float-bar-marker" style="left: calc(${markerLeft}% - 2px);"></div>
+                <div style="height: 100%; border-radius: 4px; overflow: hidden">
+                    ${floatConditions.map(cond => {
+                        if (cond.max > (itemInfo.max * 100)) {
+                            return html`<div class="market-float-bar" style="width: 0%;"></div>`; 
+                        } else {
+                            return html`<div class="market-float-bar" style="width: ${(cond.max - cond.min) * 100 / dynamicWidth}%; background-color: ${cond.min < itemInfo.min ? "none" : cond.color};"></div>`;
+                        }
+                    })}
+                </div>
+            </div>
+        `;
     }
 
     renderFloatMarketListing(): TemplateResult<1> {
