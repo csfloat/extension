aa17f1c3262eb0e8606c63c33a2f84210e336fa3
GODrums <armin.stanitzok@outlook.de>
2025-03-20 21:57:27 +0100

    Remove code from #303

--- src/lib/utils/observers.ts
+++ src/lib/utils/observers.ts
@@ -9,25 +9,3 @@ export function Observe<T>(computeObject: () => T, cb: () => any, pollRateMs = 5
         prev = now;
     }, pollRateMs);
 }
-
-type WaitForElementOptions = {
-    interval?: number;
-    maxTries?: number;
-};
-/**
- * Wait for a function to be true
- * @param compute function that returns a boolean
- * @param options options
- * @param options.interval interval in ms, default 200
- * @param options.maxTries maximum tries, default 10
- * @returns true if function returns true, false if not
- */
-export async function waitForTrue(compute: () => boolean, options?: WaitForElementOptions) {
-    const {interval = 200, maxTries = 10} = options || {};
-    let tries = 0;
-    while (!compute() && tries < maxTries) {
-        tries++;
-        await new Promise((r) => setTimeout(r, interval));
-    }
-    return tries < maxTries;
-}
