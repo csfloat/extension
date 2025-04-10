31dead097f22cfe95843ac863906c3dcac76957f
GODrums <armin.stanitzok@outlook.de>
2025-04-10 16:30:21 +0200

    Execute extension converter

---
+++ safari/csfloat-extension/csfloat-extension/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  csfloat-extension
+//
+//  Created by Armin Stanitzok on 10.04.25.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
---
+++ safari/csfloat-extension/csfloat-extensionTests/csfloat_extensionTests.swift
@@ -0,0 +1,17 @@
+//
+//  csfloat_extensionTests.swift
+//  csfloat-extensionTests
+//
+//  Created by Armin Stanitzok on 10.04.25.
+//
+
+import Testing
+@testable import csfloat_extension
+
+struct csfloat_extensionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
