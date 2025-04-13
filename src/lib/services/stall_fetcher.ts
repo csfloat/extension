10304fdecd31190d92940fe27628271ae595fee1
Izaak Prats <izaak.prats@gmail.com>
2025-04-13 08:25:15 -0400

    Fix casing in stall_fetcher.ts params

--- src/lib/services/stall_fetcher.ts
+++ src/lib/services/stall_fetcher.ts
@@ -3,9 +3,9 @@ import {FetchStall, FetchStallRequest, FetchStallResponse} from '../bridge/handl
 import {ClientSend} from '../bridge/client';
 
 class StallFetcher extends SimpleCachedQueue<FetchStallRequest, FetchStallResponse> {
-    fetch(req: FetchStallRequest, force_refresh: boolean = false): Promise<FetchStallResponse> {
+    fetch(req: FetchStallRequest, forceRefresh: boolean = false): Promise<FetchStallResponse> {
         // If force_refresh is true, clear the cache for this request before adding a new job
-        if (force_refresh) {
+        if (forceRefresh) {
             this.clearAllCache();
         }
 
